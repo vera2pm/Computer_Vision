8e21873dd20b150b4cdeefc69a693ce022615499
vera2pm <veko10@mail.ru>
2023-06-01 12:42:13 +0300

    create base cnn with layers

---
+++ src/Convolution_NN.py
@@ -0,0 +1,31 @@
+import torch.nn as nn
+import torch.optim as optim
+
+class CNN:
+    def __init__(self, num_channels):
+        self.conv1 = nn.Conv2d(in_channels=num_channels, out_channels=20, kernel_size=(5, 5))
+        self.conv2 = nn.Conv2d(in_channels=20, out_channels=50, kernel_size=(5, 5))
+        self.fc = nn.Linear(in_features=800, out_features=500)
+        self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.relu = nn.ReLU()
+
+        self.logSoftmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.pool(x)
+
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+
+        x = nn.flatten(x, 1)
+        x = self.fc(x)
+        x = self.relu(x)
+
+        res = self.logSoftmax(x)
+        return res
+
+    def train(self):
+        pass
