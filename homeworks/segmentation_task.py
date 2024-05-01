c244f332415949712f147a7bf0a3f824fe5c458d
vera2pm <veko10@mail.ru>
2024-05-02 01:21:16 +0300

    cleaner model with validation split

--- homeworks/segmentation_task.py
+++ homeworks/segmentation_task.py
@@ -8,7 +8,7 @@ import torch
 import logging
 import torchvision.transforms as transforms
 from homeworks.classification_task1 import plot_train_val
-from src.segmentation.segmentation import train_val, SegmentationDataset, blob_detection
+from src.segmentation.segmentation import train_val, SegmentationDataset, blob_detection, predict_segmentation
 from src.configs.segment_config import IMAGE_SIZE, train_path, test_path, train_files, test_files
 
 
@@ -21,7 +21,7 @@ def load_images(amount_dict_file, img_dir):
     for img_name, amount in amount_dict.items():
         img_filename = os.path.join(img_dir, img_name)
         loaded_images.append(img_filename)
-        mask_filename = os.path.join(img_dir, f'mask_{img_name}')
+        mask_filename = os.path.join(img_dir, f"mask_{img_name}")
         loaded_masks.append(mask_filename)
         loaded_target_amount.append(amount)
 
@@ -32,45 +32,57 @@ def data_loaders():
     transformers = transforms.Compose([transforms.Resize(IMAGE_SIZE)])
     print("Load train data")
     loaded_images_train, loaded_target_regions_train, train_target_amount = load_images(train_files, train_path)
-    train_data = SegmentationDataset(loaded_images_train, loaded_target_regions_train, transformers)
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=5, shuffle=True) #, collate_fn=collate_fn)
-
+    train_data = SegmentationDataset(
+        loaded_images_train, loaded_target_regions_train, train_target_amount, transformers
+    )
+    print("Split to train and validation subsets")
+    train_subset, val_subset = torch.utils.data.random_split(
+        train_data, [0.8, 0.2], generator=torch.Generator().manual_seed(1)
+    )
+    train_loader = torch.utils.data.DataLoader(train_subset, batch_size=10, shuffle=True)  # , collate_fn=collate_fn)
+    valid_loader = torch.utils.data.DataLoader(val_subset, batch_size=10, shuffle=True)
 
     print("Load test data")
     loaded_images_test, loaded_target_regions_test, test_target_amount = load_images(test_files, test_path)
-    test_data = SegmentationDataset(loaded_images_test, loaded_target_regions_test, transformers)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=5, shuffle=True) #, collate_fn=collate_fn)
+    test_data = SegmentationDataset(loaded_images_test, loaded_target_regions_test, test_target_amount, transformers)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=5, shuffle=True)  # , collate_fn=collate_fn)
 
-    # print(test_target_amount[1])
-    k=0
-    for i in range(len(test_target_amount)):
-        n_blobs = blob_detection(loaded_target_regions_test[i])
-        if n_blobs != test_target_amount[i]:
-            k+=1
-    print(f"Number of mismatches in test is {k}")
-    print(f"Len of test {len(test_target_amount)}")
+    # print(len(train_target_amount))
+    # k = 0
+    # for i in range(10):
+    #     n_blobs = blob_detection(loaded_target_regions_test[i])
+    #     if n_blobs != test_target_amount[i]:
+    #         k += 1
+    # print(f"Number of mismatches in test is {k}")
+    # print(f"Len of test {len(test_target_amount)}")
 
-    return train_loader, test_loader
+    return train_loader, valid_loader, test_loader
 
 
 def main():
     # prepare dataset
     print("prepare dataset")
     # prepare torch dataset
-    train_loader, test_loader = data_loaders()
+    train_loader, valid_loader, test_loader = data_loaders()
 
     device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
     print(f"device: {device}")
-    valid_loader = test_loader
+    # valid_loader = test_loader
 
     # check parameters
-    # print("Start train")
-    # trainval_loss_list, val_acc_list = train_val(
-    #     train_loader, valid_loader, device, num_epochs=2, learning_rate=1.0e-3, weight_decay=0.1
+    print("Start train")
+    model = None
+    # model, loss_df, metric = train_val(
+    #     train_loader, valid_loader, device, num_epochs=5, learning_rate=1.0e-4, weight_decay=0.2
     # )
+    # print(loss_df)
+    # print(metric.compute())
     # plot_train_val(trainval_loss_list, val_acc_list, title="U-NET model train-validation")
 
-
+    print("Test:")
+    n_blob_pred, metric, test_loss_list = predict_segmentation(test_loader, device, model)
+    print(test_loss_list)
+    # print(metric.compute())
     # train_loss_list, test_acc_list = train_val(
     #     train_loader, test_loader, device, num_epochs=10, learning_rate=1.0e-4, weight_decay=0.1
     # )
--- src/segmentation/segmentation.py
+++ src/segmentation/segmentation.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import matplotlib.pyplot as plt
+import pandas as pd
 import torch.optim as optim
 import torch
 from torchmetrics.detection import MeanAveragePrecision
@@ -18,18 +19,21 @@ from skimage.io import imread, imshow
 from skimage.color import rgb2gray, label2rgb
 from skimage.feature import blob_dog, blob_log, blob_doh
 
+
 class SegmentationDataset(torch.utils.data.Dataset):
     in_channels = IN_CHANNELS
     out_channels = OUT_CHANNELS
 
-    def __init__(self, imgs: list, mask: list, transforms=None):
+    def __init__(self, imgs: list, mask: list, n_blobs: list, transforms=None):
         self.transforms = transforms
         self.imgs = imgs
         self.mask = mask
+        self.n_blobs = n_blobs
 
     def __getitem__(self, idx):
         image = Image.open(self.imgs[idx])
-        mask = Image.open(self.mask[idx])
+        mask = Image.open(self.mask[idx]).convert("L")
+        n_blobs = self.n_blobs[idx]
 
         if self.transforms is not None:
             image = self.transforms(image)
@@ -38,29 +42,31 @@ class SegmentationDataset(torch.utils.data.Dataset):
         image_tensor = transforms.ToTensor()(image)
         mask_tensor = transforms.ToTensor()(mask)
 
-        return image_tensor, mask_tensor
+        return image_tensor, mask_tensor, n_blobs
 
     def __len__(self):
         return len(self.imgs)
 
 
-def blob_detection(img):
-    hotdog = imread(img)
-    hotdog = 255 - hotdog
+def blob_detection(img, i):
+    hotdog = 255 - img
+    # hotdog = imread(img)
     print(hotdog.shape)
+    hotdog = cv2.cvtColor(hotdog, cv2.COLOR_BGR2GRAY)
 
     blobs = blob_log(hotdog, max_sigma=30, num_sigma=10, threshold=0.5)
     # blobs = blob_dog(hotdog, max_sigma=100, threshold=0.5)
     print(len(blobs))
 
-    # fig, ax = plt.subplots()
-    # ax.imshow(hotdog, cmap='gray')
-    # for blob in blobs:
-    #     y, x, area = blob
-    #     ax.add_patch(plt.Circle((x, y), area * np.sqrt(2), color='r',
-    #                             fill=False))
+    fig, ax = plt.subplots()
+    ax.imshow(hotdog, cmap="gray")
+    for blob in blobs:
+        y, x, area = blob
+        ax.add_patch(plt.Circle((x, y), area * np.sqrt(2), color="r", fill=False))
     # plt.show()
+    fig.savefig(f"../data/test_ima/test_mask_{i}.jpg")
     return len(blobs)
+
 
 def blob_detection_1(img):
     """
@@ -73,31 +79,8 @@ def blob_detection_1(img):
     img = 255 - img
     print(img.shape)
 
-    # _, thresholdimage = cv2.threshold(img, 180, 255, cv2.THRESH_BINARY)
-    # print(img.shape)
-    # img = thresholdimage
-
     # Set up the detector with default parameters.
     params = cv2.SimpleBlobDetector_Params()
-
-    # Change thresholds
-    params.minThreshold = 10
-    params.maxThreshold = 200
-
-    # Filter by Area.
-    params.filterByArea = True
-    params.maxArea = 1500
-
-    # Filter by Circularity
-    params.filterByCircularity = True
-    params.minCircularity = 0.1
-
-    # Filter by Convexity
-    params.filterByConvexity = True
-    params.minConvexity = 0.87
-
-    # Filter by Inertia
-    params.filterByInertia = True
     params.minInertiaRatio = 0.01
 
     # Create a detector with the parameters
@@ -105,23 +88,18 @@ def blob_detection_1(img):
 
     # Detect blobs.
     keypoints = detector.detect(img)
-    # features = cv2.SIFT_create(contrastThreshold=0.2, edgeThreshold=5)
-    #
-    # keypoints = features.detect(img, None)
     print(len(keypoints))
 
     # Draw detected blobs as red circles.
     # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
     blank = np.zeros((1, 1))
-    blobs = cv2.drawKeypoints(
-        img, keypoints, blank, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
-    )
+    blobs = cv2.drawKeypoints(img, keypoints, blank, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     # Show keypoints
     # fig = plt.figure(figsize=(15, 5))
     # plt.imshow(blobs)
     # fig.suptitle("title")
-    cv2.imwrite(f'../key_points.jpg', blobs)
+    cv2.imwrite(f"../key_points.jpg", blobs)
     # plt.savefig(f'../key_points.jpg')
     print("Saved file")
     return keypoints
@@ -130,61 +108,60 @@ def blob_detection_1(img):
 def train_val(loader_train, loader_valid, device, num_epochs=50, learning_rate=0.001, weight_decay=0.1):
     loaders = {"train": loader_train, "valid": loader_valid}
 
-    unet = UNet(in_channels=SegmentationDataset.in_channels, out_channels=SegmentationDataset.out_channels)
-    unet.to(device)
+    model = UNet(in_channels=SegmentationDataset.in_channels, out_channels=SegmentationDataset.out_channels)
+    model.to(device)
 
     dsc_loss = DiceLoss()
+    metric = MeanAveragePrecision(iou_type="segm")
     best_validation_dsc = 0.0
 
-    optimizer = optim.Adam(unet.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
     # logger = logging
     loss_train = []
     loss_valid = []
-
+    loss_df = []
     step = 0
-
     for epoch in tqdm(range(num_epochs), total=num_epochs):
         for phase in ["train", "valid"]:
             if phase == "train":
-                unet.train()
+                model.train()
             else:
-                unet.eval()
-
-            validation_pred = []
-            validation_true = []
+                model.eval()
 
             for i, data in enumerate(loaders[phase]):
                 if phase == "train":
                     step += 1
 
-                images, masks_true = data
-                # images = list(image.to(device) for image in images)
-                # masks_true = list(mask.to(device) for mask in masks_true)
+                images, masks_true, n_blobs = data
                 images, masks_true = images.to(device), masks_true.to(device)
 
                 optimizer.zero_grad()
 
                 with torch.set_grad_enabled(phase == "train"):
-                    masks_pred = unet(images)
+                    masks_pred = model(images)
 
                     loss = dsc_loss(masks_pred, masks_true)
 
                     if phase == "valid":
                         loss_valid.append(loss.item())
-                        # y_pred_np = y_pred.detach().cpu().numpy()
-                        # validation_pred.extend([y_pred_np[s] for s in range(y_pred_np.shape[0])])
-                        # y_true_np = y_true.detach().cpu().numpy()
-                        # validation_true.extend([y_true_np[s] for s in range(y_true_np.shape[0])])
-                        # if (epoch % args.vis_freq == 0) or (epoch == num_epochs - 1):
-                        # if i * args.batch_size < args.vis_images:
-                        #     tag = "image/{}".format(i)
-                        #     num_images = args.vis_images - i * args.batch_size
-                        #     logger.image_list_summary(
-                        #         tag,
-                        #         log_images(x, y_true, y_pred)[:num_images],
-                        #         step,
+                        #
+                        # preds = [
+                        #     dict(
+                        #         masks=mask_pred,
+                        #         scores=torch.tensor([0.536]),
+                        #         labels=torch.tensor([0]),
                         #     )
+                        #     for mask_pred in masks_pred
+                        # ]
+                        # target = [
+                        #     dict(
+                        #         masks=mask_pred,
+                        #         labels=torch.tensor([0]),
+                        #     )
+                        #     for mask_pred in masks_pred
+                        # ]
+                        # metric.update(preds, target)
 
                     if phase == "train":
                         loss_train.append(loss.item())
@@ -194,37 +171,49 @@ def train_val(loader_train, loader_valid, device, num_epochs=50, learning_rate=0
                 if phase == "train" and (step + 1) % 10 == 0:
                     # log_loss_summary(logger, loss_train, step)
                     print(loss_train[-1])
-                    loss_train = []
+                    # loss_train = []
 
             if phase == "valid":
                 # log_loss_summary(logger, loss_valid, step, prefix="val_")
                 mean_dsc = np.mean(loss_valid)
-                # logger.scalar_summary("val_dsc", mean_dsc, step)
                 if mean_dsc > best_validation_dsc:
                     best_validation_dsc = mean_dsc
-                    # torch.save(unet.state_dict(), os.path.join(, "unet.pt"))
-                loss_valid = []
+                    # torch.save(model.state_dict(), os.path.join(, "model.pt"))
+                # loss_valid = []
 
+        loss_df.append({"epoch": epoch, "train": np.mean(loss_train), "test": np.mean(loss_valid)})
+        loss_train = []
+        loss_valid = []
+
+    loss_df = pd.DataFrame(loss_df)
     print("Best validation mean DSC: {:4f}".format(best_validation_dsc))
-    return loss_train, loss_valid
+    torch.save(model, "../model_inference.pt")
+    return model, loss_df, metric
 
 
-def eval_segmentation(test_data, model, device):
+def predict_segmentation(loader_test, device, model=None):
+    if model is None:
+        model = torch.load("../model_inference.pt")
     model.to(device)
     test_loss_list = []
     model.eval()
-    metric = MeanAveragePrecision(iou_type="bbox")
-    for i, (images, targets) in enumerate(test_data):
-        images = list(img.to(device) for img in images)
+    dsc_loss = DiceLoss()
+    metric = MeanAveragePrecision(iou_type="segm")
+    n_blob_pred = []
+    for i, data in enumerate(loader_test):
+        images, masks_true, n_blobs_true = data
+        images, masks_true = images.to(device), masks_true.to(device)
 
-        predictions = model(images)
-        predictions = [{k: v.to(device) for k, v in t.items()} for t in predictions]
-
-        targets = [{k: v.to(device) if isinstance(v, torch.Tensor) else v for k, v in t.items()} for t in targets]
-        print(targets)
+        masks_pred = model(images)
+        loss = dsc_loss(masks_pred, masks_true)
+        masks_pred = masks_pred.to(torch.device("cpu")).detach().numpy()
+        for i, mask in enumerate(masks_pred):
+            mask = mask.transpose(1, 2, 0)
+            mask = cv2.cvtColor(mask, cv2.COLOR_RGB2BGR)
+            n_blob_pred.append(blob_detection(mask, i))
 
         # gather the stats from all processes
-        test_loss_list.append(metric(predictions, targets))
+        test_loss_list.append(loss)
 
     # torch.set_num_threads(n_threads)
-    return predictions, metric, test_loss_list
+    return n_blob_pred, metric, test_loss_list
