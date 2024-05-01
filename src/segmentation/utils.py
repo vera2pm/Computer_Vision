50c04c29a59945ff8710105d129932c0d9880400
vera2pm <veko10@mail.ru>
2024-05-02 01:20:46 +0300

    new util function

--- src/segmentation/utils.py
+++ src/segmentation/utils.py
@@ -3,15 +3,15 @@ import os
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.spatial import distance
 from scipy.spatial import cKDTree
 
-from src.configs.segment_config import IMAGE_SIZE, train_path, test_path, train_labels_file, test_labels_file
+from src.configs.segment_config import train_path, test_path, train_labels_file, test_labels_file
 from src.utils import cv2_load2rgb
 
 
 def get_mask(image, regions):
-    image_mask = np.zeros(image.shape[:2], dtype=np.uint8)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image_mask = np.zeros(image.shape, dtype=np.uint8)
     coords_list = []
     if len(regions) == 0:
         return 255 - image_mask
@@ -29,10 +29,10 @@ def get_mask(image, regions):
     for reg in regions.keys():
         reg = regions[reg]["shape_attributes"]
         center = (int(reg["cx"]), int(reg["cy"]))
-        image_mask = cv2.circle(image_mask, center, radius, color=255, thickness=-1)
-    image_mask = 255 - image_mask
+        image_mask = cv2.circle(image_mask, center, radius, color=(255, 255, 255), thickness=-1)
+    print(image_mask.dtype)
     # img2_fg = cv2.bitwise_and(image, image, mask=image_mask)
-    # plt.imshow(img2_fg)
+    # plt.imshow(image_mask)
     # plt.show()
     return image_mask
 
@@ -44,12 +44,14 @@ def create_targets(images_dict_file, img_dir):
     loaded_target_amount = {}
     for idx, label_dict in labels_dict.items():
         img_filename = os.path.join(img_dir, label_dict["filename"])
+        print(img_filename)
         image = cv2_load2rgb(img_filename)
         # loaded_images.append(Image.open(img_filename))
         target_regions = label_dict["regions"]
         loaded_target_amount[label_dict["filename"]] = len(target_regions)
         image_mask = get_mask(image, target_regions)
         mask_path = os.path.join(img_dir, f'mask_{label_dict["filename"]}')
+        # break
         cv2.imwrite(mask_path, image_mask)
 
     print(loaded_target_amount)
@@ -58,6 +60,27 @@ def create_targets(images_dict_file, img_dir):
         json.dump(loaded_target_amount, f)
 
 
+def save_loss_summary(loss_list):
+    max(loss_list)
+
+
+def check_size(amount_dict_file, img_dir):
+    with open(amount_dict_file) as f:
+        amount_dict = json.load(f)
+    widths = []
+    heights = []
+    for img_name, amount in amount_dict.items():
+        img_filename = os.path.join(img_dir, img_name)
+        img = cv2_load2rgb(img_filename)
+        print(img.shape)
+        widths.append(img.shape[0])
+        heights.append(img.shape[1])
+    print(f"mean sizes: {np.mean(widths)}, {np.mean(heights)}")
+    print(f"min sizes: {np.min(widths)}, {np.min(heights)}")
+    print(f"max sizes: {np.max(widths)}, {np.max(heights)}")
+
+
 if __name__ == "__main__":
-    # create_targets(test_labels_file, test_path)
+    create_targets(test_labels_file, test_path)
     create_targets(train_labels_file, train_path)
+    # check_size(train_files, "../../data/segmentation_dataset/train_data/")
