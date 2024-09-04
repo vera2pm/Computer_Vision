fcd01fcec80a195d7c65c34a1c2ceee6305542c6
Vera_Kochetkova <Vera_Kochetkova@epam.com>
2024-09-04 14:34:31 +0300

    fix subset

--- src/metric_learning/preprocessing.py
+++ src/metric_learning/preprocessing.py
@@ -29,14 +29,15 @@ def preprocessing(data):
 
 def create_pos_neg_anchor_superlabel(data_table):
     for supcl in data_table["super_class_id"].unique():
-        positive_list = data_table.loc[data_table["super_class_id"] == supcl]["path"].tolist().reverse()
+        positive_list = data_table.loc[data_table["super_class_id"] == supcl]["path"].tolist()
+        positive_list.reverse()
         length = len(positive_list)
         if length % 2 != 0:
             positive_list[int((length - 1) / 2)] = positive_list[0]
         data_table.loc[data_table["super_class_id"] == supcl, "pos_list_sup"] = positive_list
 
         negative_list = data_table.loc[data_table["super_class_id"] != supcl]["path"].tolist()
-        negative_sample = random.sample(negative_list, length)
+        negative_sample = random.choices(negative_list, k=length)
         data_table.loc[data_table["super_class_id"] == supcl, "neg_list_sup"] = negative_sample
 
     return data_table
@@ -45,24 +46,40 @@ def create_pos_neg_anchor_superlabel(data_table):
 def save_data(data_table, save_path):
     shape_df = preprocessing(data_table)
     shape_df = create_pos_neg_anchor_superlabel(shape_df)
-    shape_df.to_csv(save_path)
+    shape_df.to_csv(save_path, index=False)
 
 
 def read_data():
     train_data = pd.read_table("../../data/Stanford_Online_Products/Ebay_train.txt", sep=" ")
     test_data = pd.read_table("../../data/Stanford_Online_Products/Ebay_test.txt", sep=" ")
-    shape_df = preprocessing(train_data)
+    # train_data = preprocessing(train_data)
 
-    gss = GroupShuffleSplit(test_size=0.33, n_splits=2, random_state=42).split(shape_df, groups=shape_df["class_id"])
+    X_subtrain_inds, X_subtest_inds = next(GroupShuffleSplit(test_size=0.2, n_splits=1, random_state=42).split(
+        train_data, groups=train_data["class_id"]
+    ))
 
-    X_subtrain_inds, X_subtest_inds, X_train_inds, X_test_inds = next(gss)
+    gss = GroupShuffleSplit(test_size=0.33, n_splits=1, random_state=42)
+    X_train_inds, X_test_inds = next(gss.split(
+        train_data, groups=train_data["class_id"]
+    ))
+
+    X_subtrain_inds, X_subtest_inds = next(gss.split(
+        train_data.iloc[X_subtest_inds], groups=train_data.iloc[X_subtest_inds]["class_id"]
+    ))
     print(len(X_subtrain_inds), len(X_subtest_inds), len(X_train_inds), len(X_test_inds))
-    shape_df.iloc[X_train_inds].to_csv("../../data/Stanford_Online_Products/Ebay_train_train_preproc.csv", index=False)
-    shape_df.iloc[X_test_inds].to_csv("../../data/Stanford_Online_Products/Ebay_train__val_preproc.csv", index=False)
-    shape_df.to_csv("../../data/Stanford_Online_Products/Ebay_train_preproc.csv", index=False)
+    print(train_data.iloc[X_subtrain_inds]["super_class_id"].nunique())
+    print(train_data.iloc[X_subtest_inds]["super_class_id"].nunique())
 
-    shape_df = preprocessing(test_data)
-    shape_df.to_csv("../../data/Stanford_Online_Products/Ebay_test_preproc.csv", index=False)
+    save_data(train_data.iloc[X_subtrain_inds],
+        "../../data/Stanford_Online_Products/Ebay_subtrain_preproc.csv")
+    save_data(train_data.iloc[X_subtest_inds], "../../data/Stanford_Online_Products/Ebay_subval_preproc.csv")
+
+    save_data(train_data.iloc[X_train_inds], "../../data/Stanford_Online_Products/Ebay_train_train_preproc.csv")
+    save_data(train_data.iloc[X_test_inds], "../../data/Stanford_Online_Products/Ebay_train__val_preproc.csv")
+    save_data(train_data, "../../data/Stanford_Online_Products/Ebay_train_preproc.csv")
+
+    test_data = preprocessing(test_data)
+    test_data.to_csv("../../data/Stanford_Online_Products/Ebay_test_preproc.csv", index=False)
 
 
 if __name__ == "__main__":
