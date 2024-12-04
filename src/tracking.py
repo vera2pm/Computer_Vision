7f1b5cdc7591669254589e67e812e96ce151c213
Vera_Kochetkova <Vera_Kochetkova@epam.com>
2024-12-04 12:44:06 +0300

    loop in video

--- src/tracking.py
+++ src/tracking.py
@@ -2,16 +2,18 @@ from deep_sort_realtime.deepsort_tracker import DeepSort
 from ultralytics import YOLO
 
 
-def main():
+def main(video):
     # Load a model
     object_detector = YOLO("yolov8n.pt")
     tracker = DeepSort(max_age=5, embedder="clip_ViT-B/32")
-    bbs = object_detector.detect(frame)
-    tracks = tracker.update_tracks(
-        bbs, frame=frame
-    )  # bbs expected to be a list of detections, each in tuples of ( [left,top,w,h], confidence, detection_class )
-    for track in tracks:
-        if not track.is_confirmed():
-            continue
-        track_id = track.track_id
-        ltrb = track.to_ltrb()
+    for frame in video:
+        results = object_detector.track(frame)
+        bbs = results[0].boxes.xywh  # .cpu()
+        tracks = tracker.update_tracks(
+            bbs, frame=frame
+        )  # bbs expected to be a list of detections, each in tuples of ( [left,top,w,h], confidence, detection_class )
+        for track in tracks:
+            if not track.is_confirmed():
+                continue
+            track_id = track.track_id
+            ltrb = track.to_ltrb()
