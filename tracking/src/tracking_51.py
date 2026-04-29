37368fc542b11e683b80db12b9ec7fe348fc1c94
vera2pm <veko10@mail.ru>
2026-04-30 00:49:09 +0300

    update dataset

--- tracking/src/tracking_51.py
+++ tracking/src/tracking_51.py
@@ -1,24 +1,22 @@
 import os
 import sys
+from pathlib import Path
+
+import cv2
 import motmetrics as mm
 import numpy as np
 import pandas as pd
 from sahi.utils.ultralytics import download_yolo11n_model
-
-# from torchmetrics.detection import MeanAveragePrecision
 from tqdm import tqdm
-import numpy as np
-import cv2
-from pathlib import Path
 
 sys.path.append("../")
-from boxmot import BotSort
-from ultralytics import YOLO
-from ultralytics.utils.ops import xyxy2ltwh, ltwh2xywh
-from sahi.predict import get_sliced_prediction
-from sahi import AutoDetectionModel
 import fiftyone as fo
 import fiftyone.utils.video as fouv
+from boxmot import BotSort
+from sahi import AutoDetectionModel
+from sahi.predict import get_sliced_prediction
+from ultralytics import YOLO
+from ultralytics.utils.ops import ltwh2xywh, xyxy2ltwh
 
 print(f"Your FO version is: {fo.__version__}")
 
@@ -27,92 +25,102 @@ mv_dir = os.path.join(root_folder, "mot20_mvs")
 model_path = "models/yolo11n.pt"
 download_yolo11n_model(model_path)
 
-
-def create_videos():
-    for movi in ["MOT20/train/MOT20-01/"]:  # "MOT20/train/MOT20-05/", "MOT20/test/MOT20-06/",
-        print(movi)
-        mov_in = os.path.join(root_folder, movi, f"img1/%06d.jpg")
-        name = movi.split("/")[2]
-        mov_out = os.path.join(mv_dir, f"movi_{name}.mp4")
-        fouv.reencode_video(
-            mov_in,
-            mov_out,
-            verbose=False,
-        )
-
-
 GT_COLUMNS = ["frame", "id", "x0", "y0", "w", "h", "flag", "class", "visibility"]
 GT_CLASSES = ["na", "person", "na", "na", "na", "na", "vehicle", "person", "na", "na", "na", "occluded", "na", "crowd"]
 
 
-def save_labels_from_sample(sample):
-    # for sample in dataset[:1]:
-    # Match image filename root
-    video_stem = os.path.splitext(os.path.basename(sample.filepath))[0]
-    print(video_stem)
-    os.makedirs(f"../data/train_mot20/{video_stem}/labels")
+class Dataset:
+    def __init__(self):
+        self.list_images2video_dirs = ["MOT20/train/MOT20-01/"]  # , "MOT20/train/MOT20-05/", "MOT20/test/MOT20-06/"]
 
-    for i in range(len(sample.frames)):
-        frame_number = i + 1
-        f = sample.frames[frame_number]
-        output_file = open(f"../data/train_mot20/{video_stem}/labels/{frame_number:06d}.txt", "w")
-        for det in f.gt.detections:
-            label = det.label  # string
-            class_id = 0  # adjust this based on label if multi-class
-            ltwhn = np.array(det.bounding_box)
-            # print(label)
+    def create_videos(self):
+        for movi in self.list_images2video_dirs:
+            print(movi)
+            mov_in = os.path.join(root_folder, movi, f"img1/%06d.jpg")
+            name = movi.split("/")[2]
+            mov_out = os.path.join(mv_dir, f"movi_{name}.mp4")
+            fouv.reencode_video(
+                mov_in,
+                mov_out,
+                verbose=False,
+            )
 
-            # Convert from [x, y, w, h] (normalized) to YOLO [x_center, y_center, w, h]
-            xc, yc, w, h = list(ltwh2xywh(ltwhn))
-            output_file.write(f"{class_id} {xc:.6f} {yc:.6f} {w:.6f} {h:.6f}\n")
-        output_file.close()
+    def save_labels_from_sample(self, sample):
+        # for sample in dataset[:1]:
+        # Match image filename root
+        video_stem = os.path.splitext(os.path.basename(sample.filepath))[0]
+        print(video_stem)
+        os.makedirs(os.path.join(root_folder, f"train_mot20/{video_stem}/labels"))
 
+        for i in range(len(sample.frames)):
+            frame_number = i + 1
+            f = sample.frames[frame_number]
+            output_file = open(
+                os.path.join(root_folder, f"train_mot20/{video_stem}/labels/{frame_number:06d}.txt"), "w"
+            )
+            for det in f.gt.detections:
+                label = det.label  # string
+                class_id = 0  # adjust this based on label if multi-class
+                ltwhn = np.array(det.bounding_box)
+                # print(label)
 
-def add_ground_truth():
-    dataset = fo.load_dataset("mot20")
-    view = dataset.skip(2)
-    frames = view.to_frames(sample_frames=True)
-    df = pd.read_table(
-        os.path.join(root_folder, "MOT20/train/MOT20-03/gt/gt.txt"),
-        header=None,
-        sep=",",
-    )
-    view = dataset.last()
-    df.columns = GT_COLUMNS
-    imw = view.metadata.frame_width
-    imh = view.metadata.frame_height
-    df = df.sort_values(by="frame")
-    frames = df["frame"].astype("int").unique()
+                # Convert from [x, y, w, h] (normalized) to YOLO [x_center, y_center, w, h]
+                xc, yc, w, h = list(ltwh2xywh(ltwhn))
+                output_file.write(f"{class_id} {xc:.6f} {yc:.6f} {w:.6f} {h:.6f}\n")
+            output_file.close()
 
-    for i in tqdm(frames):
-        f = view.frames[int(i)]
-        try:
-            if f.gt is None:
-                f.gt = fo.Detections()
-        except AttributeError:
-            f["gt"] = fo.Detections()
-        f.gt = fo.Detections()
-        df_frame = df.loc[df.frame == i]
-        for i, row in df_frame.iterrows():
-            track_id = row["id"]
-            label = GT_CLASSES[int(row["class"])]
-            ltwh = np.array(row[["x0", "y0", "w", "h"]])
-            ltwhn = np.array([ltwh[0] / imw, ltwh[1] / imh, ltwh[2] / imw, ltwh[3] / imh])
-            det = fo.Detection(label=label, bounding_box=ltwhn, index=track_id)
-            f.gt.detections.append(det)
-        f.save()
+    def add_ground_truth(self, view, gt_path="MOT20/train/MOT20-03/gt/gt.txt"):
+        # dataset = fo.load_dataset("mot20")
+        # view = dataset.skip(2)
+        # frames = view.to_frames(sample_frames=True)
+        df = pd.read_table(
+            os.path.join(root_folder, gt_path),
+            header=None,
+            sep=",",
+        )
+        # view = dataset.last()
+        df.columns = GT_COLUMNS
+        imw = view.metadata.frame_width
+        imh = view.metadata.frame_height
+        df = df.sort_values(by="frame")
+        frames = df["frame"].astype("int").unique()
 
+        for i in tqdm(frames):
+            f = view.frames[int(i)]
+            try:
+                if f.gt is None:
+                    f.gt = fo.Detections()
+            except AttributeError:
+                f["gt"] = fo.Detections()
+            f.gt = fo.Detections()
+            df_frame = df.loc[df.frame == i]
+            for i, row in df_frame.iterrows():
+                track_id = row["id"]
+                label = GT_CLASSES[int(row["class"])]
+                ltwh = np.array(row[["x0", "y0", "w", "h"]])
+                ltwhn = np.array([ltwh[0] / imw, ltwh[1] / imh, ltwh[2] / imw, ltwh[3] / imh])
+                det = fo.Detection(label=label, bounding_box=ltwhn, index=track_id)
+                f.gt.detections.append(det)
+            f.save()
 
-def create_dataset():
-    # create_videos()
-    # dataset = fo.load_dataset("mot20")
-    # dataset.delete()
-    dataset = fo.Dataset.from_dir(dataset_dir=mv_dir, dataset_type=fo.types.VideoDirectory)
-    dataset.ensure_frames()
-    dataset.name = "mot20"
-    dataset.persistent = True
-    sample = dataset.first()
-    print(sample)
+    def add_new_sample(self, path="movi_MOT20-01.mp4", gt_path="MOT20/train/MOT20-01/gt/gt.txt"):
+        dataset = fo.load_dataset("mot20")
+        sample = fo.Sample(filepath=os.path.join(mv_dir, path))
+        dataset.add_sample(sample)
+        view = dataset.skip(3)
+        frames = view.to_frames(sample_frames=True)
+        Dataset().add_ground_truth(dataset.last(), gt_path)
+
+    def create_dataset(self):
+        self.create_videos()
+        # dataset = fo.load_dataset("mot20")
+        # dataset.delete()
+        dataset = fo.Dataset.from_dir(dataset_dir=mv_dir, dataset_type=fo.types.VideoDirectory)
+        dataset.ensure_frames()
+        dataset.name = "mot20"
+        dataset.persistent = True
+        sample = dataset.first()
+        print(sample)
 
 
 def tracking_with_sliced_prediction(view):
@@ -188,8 +196,6 @@ def tracking_yolo(view, model, tag):
     results = model.track(mov_path, device="mps", stream=True, imgsz=1280, conf=0.4, iou=0.6)
 
     for frm, res in tqdm(enumerate(results), total=(len(view.first().frames))):
-        if frm <= 1669:
-            continue
         f = view.first().frames[frm + 1]
         try:
             f[tag] = fo.Detections()
@@ -258,7 +264,7 @@ def main():
     dataset = fo.load_dataset("mot20")
     # save_labels_from_sample(dataset.last())
     tag = "yolo_best_nms"
-    view = dataset[2:3]
+    view = dataset[4:5]
     # model = YOLO("yolo11s.pt")  # yolov8s-worldv2.pt  , yolo11s.pt
     model = YOLO("tracking/runs/detect/train/weights/best.pt")
     tracking_yolo(view, model, tag)
@@ -269,15 +275,15 @@ def main():
 
 
 if __name__ == "__main__":
-    # create_dataset()
-    # add_ground_truth()
-
     dataset = fo.load_dataset("mot20")
     main()
 
     session = fo.launch_app(dataset)
-
     session.wait()
+
     # print(view.first().filepath)
     # print(view.count)
     # save_results(view, "yolo11_trained_10")
+
+    # model = YOLO("runs/detect/train9/weights/best.pt")
+    # metrics = model.val(conf=0.5)
